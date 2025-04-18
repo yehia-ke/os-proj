241aaabeacddfdfe40c87e03e2d85f1967563780
RemoteShift <alloush-2004@hotmail.com>
2025-04-18 17:14:49 +0200

    Refactor run_fcfs function to improve process handling and update return type

--- src/fcfs/fcfs_scheduler.c
+++ src/fcfs/fcfs_scheduler.c
@@ -1,6 +1,7 @@
 #include "fcfs_scheduler.h"
 #include <stdio.h>
 
+PCB* running_process = NULL;  //Pointer to the currently running process
 PCB* processes[20];  //Turn this into a Queue
 int num_processes = 0;  //Number of processes in the array
 
@@ -13,28 +14,21 @@ void add_process(PCB* process) {  //Function to add a process to the array
     }
 }
 
-void run_fcfs() {
+char* run_fcfs() {
+
+    // Check if the running process is not NULL
+    // if true, get the instruction from the running process
+    // If returns NULL, Update num_processes & Free the process from memory. Else,
+    // Return the instruction
+    // else
 
     // Check if there are any processes in the queue
-    // If not, return
+    // If not, return NULL
     // else
 
     // Dequeue a process from ready queue
     // Update the process state to "Running"
-    // Execute the process until it finishes
-    // Update num_processes
-    // Free the process from memory
-    // Recall the function to check for more processes
-
-
-    /*PCB* process = processes[i];  //We grab the current process from the array
-    printf("Running Process %d\n", process->pid);  //Show which process is being run right now
-    set_state(process, "Running");  //Update the process state to "Running" in both: Memory and the PCB struct
-    char* instruction;  //Declare a variable to hold each instruction
-    while ((instruction = get_instruction(process)) != NULL) {  //Get the next instruction based on the process’s PC(If there's an instruction, run the loop, If there's no more, exit the loop)
-        printf("Executing: %s\n", instruction);  //Print the instruction so you can see what’s running
-        process->pc++;  //Move the PC to the next instruction so next time it gets the following one.
-    }
-    printf("Process %d Finished\n\n", process->pid);  //Print that the process has finished
-    free_process(process);  //Free the memory of that process (both from memory and from the heap)*/
+    // Set the running process to the dequeued process
+    // Get instruction from the process
+    // Return the instruction
 }
