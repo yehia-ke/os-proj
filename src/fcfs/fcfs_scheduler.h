d3d63ef4c66401251e3bc39036093ef72f5ea603
RemoteShift <alloush-2004@hotmail.com>
2025-04-19 18:07:34 +0200

    Refactor run_fcfs and run_mlfq functions to return void instead of char*, and update instruction execution logic

--- src/fcfs/fcfs_scheduler.h
+++ src/fcfs/fcfs_scheduler.h
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 #include "../pcb/pcb.h"
 #include "../queue/queue.h"
+//#include "../interpreter/interpreter.h"
 
 void initialize_fcfs();
-char* run_fcfs();
+void run_fcfs();
 void fcfs_add_process(PCB* process);
 
 #endif
